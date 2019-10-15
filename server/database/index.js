136f326343a4aeba7535796340cfd0e0278df679
thomasmudge <alephcult@gmail.com>
2019-10-15 12:45:02 -0500

    Fleshed out `/server/database/index.js` & `/server/helpers/github.js` some more

--- server/database/index.js
+++ server/database/index.js
@@ -12,7 +12,7 @@ mongoose.connect(DB_URI, { useNewUrlParser: true })
 const repoSchema = new mongoose.Schema({
   // TODO: your schema here!
   id: Number,
-  name: String, // this will be owner.login
+  name: String, // this will be owner.login (data.json)
   description: String, // rest are same
   html_url: String,
   repos_url: String,
@@ -20,19 +20,44 @@ const repoSchema = new mongoose.Schema({
 
 const Repo = mongoose.model('Repo', repoSchema);
 
-const saveRepo = () => {
+const saveRepo = (githubObject) => {
   // TODO: Your code here
   // This function should save a repo to the MongoDB
-
+  const repoCollection = [];
+  const parsedRepos = JSON.parse(githubObject.body);
   // make a collection (array?)
   // parse the repo?
   // loop through parsed repos?
+  for (let i = 0; i < parsedRepos.length; i + 1) {
+    const repoObj = {};
+    repoObj.id = parsedRepos[i].id;
+    repoObj.name = parsedRepos[i].owner.login;
+    repoObj.description = parsedRepos[i].description;
+    repoObj.html_url = parsedRepos[i].html_url;
+    repoObj.repos_url = parsedRepos[i].repos_url;
+  
+    const repo = new Repo(repoObj);
+
+    repo.save((err) => {
+      if (err) {
+        console.log('Error: ', err);
+      }
+    });
+    repoCollection.push(repoObj);
+  }
 };
 
-const getTop25Repos = () => {
+const getTop25Repos = (callback) => {
   // TODO: Your code here
   // This function should get the repos from mongo
   // Repo.find(something)
+  Repo.find((err, repos) => {
+    if (err) {
+      console.log('Error: ', err);
+    } else {
+      callback(repos);
+    }
+  }).limit(25).sort({ 'name': 1 });
 };
 
 module.exports.SORTING_BY_FIELD = SORTING_BY_FIELD;
--- server/helpers/github.js
+++ server/helpers/github.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const config = require('../config');
+const db = require('../database');
 const testData = require('../data.json');
 
 const getReposByUsername = () => {
@@ -9,14 +10,21 @@ const getReposByUsername = () => {
   // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   const options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       // https://developer.github.com/v3/#user-agent-required
       'User-Agent': 'request',
       'Authorization': `token ${config.GITHUB_TOKEN}`,
     },
   };
-
+request.get(options, (err, githubObject){
+  if(err){
+    console.log('Error: ', err);
+  } else {
+    console.log(null, githubObject.body);
+    callback(githubObject);
+  }
+});
 };
 
 module.exports.getReposByUsername = getReposByUsername;
